2e74053f0d20a54a6a6c8c93869db965326e9e71
Addy Hebou <addyhebou@gmail.com>
2023-01-15 12:52:34 -0800

    FEAT: Added the header and button

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -1,4 +1,7 @@
+import { SAINT_STEPH_SOCIALS, Social } from '../Constants/Socials';
 import '../Styles/Banner.scss';
+import HeaderWithBtn from './HeaderWithBtn';
+import Socials from './Socials';
 export default function Banner({ dimmed }: { dimmed: boolean }) {
   const dimStyles = dimmed ? 'rgba(0,0,0,.5)' : '';
 
@@ -8,6 +11,17 @@ export default function Banner({ dimmed }: { dimmed: boolean }) {
   return (
     <div className="Banner" style={{ backgroundColor: dimStyles }}>
       <img className="BannerImage" src={url} alt="Saint Steph in the studio" />
+      <HeaderWithBtn
+        title={'Saint Steph'}
+        buttonText={'Listen Now'}
+        link={
+          SAINT_STEPH_SOCIALS?.[
+            SAINT_STEPH_SOCIALS.findIndex(
+              (social: Social) => social.name.toLowerCase() === 'spotify'
+            )
+          ].link
+        }
+      />
     </div>
   );
 }
---
+++ src/Components/HeaderWithBtn.tsx
@@ -0,0 +1,25 @@
+import '../Styles/HeaderWithBtn.scss';
+
+export default function HeaderWithBtn({
+  title,
+  buttonText,
+  link,
+}: {
+  title: string;
+  buttonText: string;
+  link: string;
+}) {
+  return (
+    <div className="HeaderWithBtn">
+      <h1 className="SiteTitle">{title}</h1>
+      <a
+        className="SiteButton"
+        href={link}
+        target={'_blank'}
+        referrerPolicy="no-referrer"
+      >
+        {buttonText}
+      </a>
+    </div>
+  );
+}
