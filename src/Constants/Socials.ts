172746bdaa435f249d4afee17e8b0fb1443e717a
Addy Hebou <addyhebou@gmail.com>
2023-11-22 00:29:32 -0500

    FEAT: Updated Spotify link

--- src/Constants/Socials.ts
+++ src/Constants/Socials.ts
@@ -21,7 +21,7 @@ export const SAINT_STEPH_SOCIALS: Social[] = [
     name: 'Spotify',
     imageURL:
       'https://storage.googleapis.com/saint_steph_site_photos/image%207.png',
-    link: 'https://open.spotify.com/playlist/3BEcw7WkLRmutsBUqZsHQ4?si=184a965ff9354756&nd=1',
+    link: 'https://open.spotify.com/artist/2Bu7cY0qNdHDQuPE7woB3k?si=X6YGiamXT-Sj7TfxLNN5hQ',
   } as Social,
   {
     name: 'Apple Music',
